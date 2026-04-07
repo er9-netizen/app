f2cbbb36533f9c36fa41aa00f17997f46927d3a8
Your Name <your@email.com>
2026-04-07 19:43:19 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+from openai import OpenAI
+
+app = Flask(__name__)
+
+client = OpenAI(api_key="AIzaSyCdJRJCzznS8hiRdl84_M1mPiwlYGdbK3Y")
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    response = ""
+    if request.method == "POST":
+        user_input = request.form["message"]
+
+        completion = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "user", "content": user_input}
+            ]
+        )
+
+        response = completion.choices[0].message.content
+
+    return render_template("index.html", response=response)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
